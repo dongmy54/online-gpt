f7904534375405d840d67773264f90e6746ca0f5
dongmingyan <dongmingyan01@gmail.com>
2023-05-21 12:23:45 +0800

    将样式表js单独抽成style.js

--- content.js
+++ content.js
@@ -363,15 +363,3 @@ chatInputWrapper.appendChild(clearButton);
 chatInputWrapper.classList.add("input-wrapper-with-clear-button");
 
 
-// 获取样式表文件的绝对路径
-const styleSheetPath = chrome.extension.getURL('style.css');
-
-// 创建一个<link>元素来加载样式表
-const linkElement = document.createElement('link');
-linkElement.rel = 'stylesheet';
-linkElement.href = styleSheetPath;
-
-// 将<link>元素附加到页面中
-document.head.appendChild(linkElement);
-
-
