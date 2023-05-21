7262bf669fa3fceae4e49db4789176c310477e3c
dongmingyan <dongmingyan01@gmail.com>
2023-05-21 19:57:17 +0800

    优化api_key 配置页面的交互

--- config.js
+++ config.js
@@ -1,17 +1,37 @@
-// config.js
 document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('config-form');
+  // Get the API key from storage
+  chrome.storage.local.get('OPENAI_API_KEY', function(result) {
+    const apiKey = result.OPENAI_API_KEY;
 
+    // Check if API key exists
+    if (apiKey) {
+      // Set the API key in the input field
+      const apiKeyInput = document.getElementById('api-key');
+      apiKeyInput.value = apiKey;
+    }
+  });
+
+  // Handle the form submission
+  const form = document.getElementById('config-form');
   form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // 获取用户输入的 API 密钥
-    const apiKey = document.getElementById('api-key').value;
+    // Get the API key from the input field
+    const apiKeyInput = document.getElementById('api-key');
+    const apiKey = apiKeyInput.value;
 
-    // 将 API 密钥保存到本地存储
+    // Save the API key to storage
     chrome.storage.local.set({ 'OPENAI_API_KEY': apiKey }, function() {
-      console.log('API key saved.');
+      // 保存成功后显示保存成功的消息
+      const saveSuccessMessage = document.getElementById('save-success-message');
+      saveSuccessMessage.style.display = 'block';
+
+      // 延迟一段时间后隐藏保存成功的消息
+      setTimeout(function() {
+        saveSuccessMessage.style.display = 'none';
+      }, 2000);
     });
   });
 });
+
 
