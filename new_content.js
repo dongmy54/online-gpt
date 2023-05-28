1dad580697e1d9a2644e6af45bacfa2149e40965
dongmingyan <dongmingyan01@gmail.com>
2023-05-28 22:41:12 +0800

    接入流式接入gpt成功

--- new_content.js
+++ new_content.js
@@ -38,7 +38,9 @@ function sendMessage() {
   if (message !== "") {
     addMessage(message, true);
     clearMessage();
-    receiveReply(message);
+    // receiveReply(message);
+    // addReplyMessage("你好，我收到你的消息：" + message)
+    requestGptStream(message);
   }
 }
 
@@ -50,6 +52,31 @@ function receiveReply(message) {
   }, 1000);
 }
 
+// 打字机效果
+function typeWriterEffect(text, element) {
+  var index = 0;
+  var speed = 50; // 打字速度
+  var timer = setInterval(function () {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      // 动态调整滚动条
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+      index++;
+    }
+  }, speed);
+}
+
+// 最后的回复span元素
+function lastReplySpanElement(){
+  var receivedMessages = chatMessages.getElementsByClassName("chat-message received");
+  // 获取最后一条 chat-message received 元素
+  var lastReceivedMessage = receivedMessages[receivedMessages.length - 1];
+  messageBubble = lastReceivedMessage.querySelector('.message-bubble');
+  textSpan = messageBubble.querySelector('span');
+  return textSpan;
+}
+
+// 用于添加发送的消息
 function addMessage(text, isSent) {
   var messageContainer = document.createElement("div");
   messageContainer.classList.add("chat-message");
@@ -78,6 +105,41 @@ function addMessage(text, isSent) {
   chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
+let firstStageReply = true;  // 是否为第一个阶段回复 - 控制是否新增头像和回复文本span
+let replyComplete   = false; // 回复是否完成 - 控制光标展示和生成动画
+
+// 用于添加gpt回复的消息
+function addReplyMessage(text) {
+  if (firstStageReply){ // 新增
+    var messageContainer = document.createElement("div");
+    messageContainer.classList.add("chat-message");
+    messageContainer.classList.add("received");
+
+    var avatarElement = document.createElement("div");
+    avatarElement.classList.add("avatar");
+
+    var textElement = document.createElement("span");
+    textElement.classList.add("text");
+
+    var messageBubble = document.createElement("div");
+    messageBubble.classList.add("message-bubble");
+    messageBubble.appendChild(textElement);
+
+    messageContainer.appendChild(avatarElement);
+    messageContainer.appendChild(messageBubble);
+
+    chatMessages.appendChild(messageContainer);
+    firstStageReply = false;
+  } else {
+    textElement = lastReplySpanElement();
+  }
+
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+
+  // 打字机效果
+  typeWriterEffect(text, textElement);
+}
+
 messageInput.addEventListener("input", function () {
   this.style.height = "auto";
   this.style.height = this.scrollHeight + "px";
@@ -102,4 +164,80 @@ const defaultChatMsgs = `hello,我是多啦A梦!快来和和我玩吧：
 4.我能帮你提取网页的要点，生成简洁的摘要。让我们快速捕捉信息的精华，一起掌握重点！
 `;
 addMessage(defaultChatMsgs);
+
+
+let resultText = '';
+
+function processStreamData(data) {
+  const results = data.trim().split('\n');
+  results.forEach((result) => {
+    if (result.substr(6) == '[DONE]') {
+      console.log('数据传输结束');
+      firstStageReply = false;
+      replyComplete   = true;
+      // 进行数据传输结束后的逻辑处理
+    } else {
+      try {
+        const resultObj = JSON.parse(result.substr(6));
+        const reply = resultObj.choices[0]?.delta?.content;
+        if (reply) {
+          resultText += reply;
+          console.log(resultText);
+          // 在页面中展示回复消息
+          addReplyMessage(reply);
+        } 
+      } catch (error) {
+        console.log(`当前数据：----${result.substr(6)}----`);
+        console.error("解析数据时出错:", error);
+      }
+    }
+  });
+}
+
+function requestGptStream(message) {
+  firstStageReply = true;
+  replyComplete   = false;
+  fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer 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",
+      "Accept": "text/event-stream",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "model": "gpt-3.5-turbo",
+      "messages": [{"role": "user", "content": message}],
+      "stream": true
+    })
+  })
+  .then(response => {
+    const stream = response.body;
+    const reader = stream.getReader();
+    let buffer = '';
+    
+    function read() {
+      reader.read().then(({ done, value }) => {
+        if (value) {
+          const text = new TextDecoder("utf-8").decode(value);
+          buffer += text;
+          const results = buffer.split('\n\n');
+          buffer = results.pop();
+          results.forEach((result) => {
+            processStreamData(result);
+          });
+        }
+        if (!done) {
+          read();
+        }
+      });
+    }
+    read();
+  });
+}
+
+// 示例用法
+// const userInput = "世界上有多少人";
+// showUserMessage(userInput);
+//requestGptStream(userInput);
+
 
